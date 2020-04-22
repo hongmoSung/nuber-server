c28114be64ac2f72339d0405d00f833f5cc3d165
hongmo sung <tesung30@naver.com>
2020-04-22 23:35:30 +0900

    Projoect Setup Graphql Yoga and Express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("works!");
+import app from "./app";
+import { Options } from "graphql-yoga";
+
+const PORT: number | string = process.env.PORT || 4000;
+const PLAYGROUND_ENDPOINT: string = "/playground";
+const GRAPHQL_ENDPOINT: string = "/graphql";
+
+const appOtions: Options = {
+  port: PORT,
+  playground: PLAYGROUND_ENDPOINT,
+  endpoint: GRAPHQL_ENDPOINT,
+};
+
+const handleAppStart = (): void => console.log(`Listening on port ${PORT}`);
+
+app.start(appOtions, handleAppStart);
